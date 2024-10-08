5094c5619bac5532e1ab5ffadaab823f48dfd299
Chris Erin <chris.erin@gmail.com>
2024-10-08 15:30:46 +0000

    feat: key q for quit

--- ui.go
+++ ui.go
@@ -29,7 +29,7 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 		switch msg.String() {
-		case "ctrl+c", "esc":
+		case "ctrl+c", "esc", "q":
 			return m, tea.Quit
 		case "a":
 			return m, tea.EnterAltScreen
